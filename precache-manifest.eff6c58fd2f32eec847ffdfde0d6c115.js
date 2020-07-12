93b16bca012ec9d4649892d02d494fc8ab13422d
halosatrio <loredana@disroot.org>
2020-07-12 15:07:58 +0700

    initial commit

---
+++ precache-manifest.eff6c58fd2f32eec847ffdfde0d6c115.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b2bbab7f9b0dab2c9d891be724f9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a0dd34c27ff21bc657",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "95ed7d0f22bc72b5aea2",
+    "url": "/static/css/main.6cfc4c89.chunk.css"
+  },
+  {
+    "revision": "76a0dd34c27ff21bc657",
+    "url": "/static/js/2.5f389721.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/static/js/2.5f389721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ed7d0f22bc72b5aea2",
+    "url": "/static/js/main.c0ecf971.chunk.js"
+  },
+  {
+    "revision": "dca7527c5c452e5eef76",
+    "url": "/static/js/runtime-main.634bbd6c.js"
+  },
+  {
+    "revision": "bd6bedc2daf8aafca9c3508884ebb743",
+    "url": "/static/media/hero.bd6bedc2.jpeg"
+  },
+  {
+    "revision": "6014bcae67a6cd32b00a0cc6f63c7b78",
+    "url": "/static/media/hero1.6014bcae.jpg"
+  },
+  {
+    "revision": "b42086e706f6b134fe652d6073eb2791",
+    "url": "/static/media/hero2.b42086e7.jpeg"
+  },
+  {
+    "revision": "1b9ed0f58d57e68602463a5061e4fdb5",
+    "url": "/static/media/icon-book.1b9ed0f5.png"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "12c2e5c9390378f836554f923de3c629",
+    "url": "/static/media/icon-house.12c2e5c9.png"
+  },
+  {
+    "revision": "c737096978f66a1f6a371f35582cf73a",
+    "url": "/static/media/icon-social.c7370969.png"
+  },
+  {
+    "revision": "e38261c215b4498088b390c531f46296",
+    "url": "/static/media/katalog-banner.e38261c2.jpg"
+  }
+]);
